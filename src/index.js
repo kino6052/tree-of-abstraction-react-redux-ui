82d33a9bb218c99f067084625b4d32c0db34b2e2
kino6052 <kino6052@colorado.edu>
2018-10-30 09:44:46 -0600

    Add root saga

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
 import reducer from './reducers/root'
 import generateTree from './generateTree'
 import Node from './containers/Node'
-
+import { rootSaga } from './sagas'
+const sagaMiddleware = createSagaMiddleware();
 const tree = generateTree()
 const store = createStore(
-    reducer,
-    tree,
-    compose(
-      applyMiddleware(thunk),
-      window.devToolsExtension ? window.devToolsExtension() : f => f
-    )
-  );
+  reducer,
+  tree,
+  compose(
+    applyMiddleware(thunk),
+    applyMiddleware(sagaMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+sagaMiddleware.run(rootSaga);
 
 render(
   <Provider store={store}>
